808affeb12f22dcc2ac73a4ccd9cd63ce08ac81d
Harindu Egalla <harindu.e@asyncmetric.com>
2024-05-02 19:28:29 +0530

    auth pages done

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,15 @@
 import { SignIn } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "NoteSwift - Sign In",
+};
 
 export default function Page() {
-  return <SignIn path="/sign-in" />;
+  return (
+    <SignIn
+      appearance={{ variables: { colorPrimary: "#0F172A" } }}
+      path="/sign-in"
+    />
+  );
 }
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,15 @@
 import { SignUp } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "NoteSwift - Sign Up",
+};
 
 export default function Page() {
-  return <SignUp path="/sign-up" />;
+  return (
+    <SignUp
+      appearance={{ variables: { colorPrimary: "#0F172A" } }}
+      path="/sign-up"
+    />
+  );
 }
