4fd676637524a789a66e1d1603b37a9568aa3c71
Harindu Egalla <harindu.e@asyncmetric.com>
2024-05-02 18:35:44 +0530

    added shadcn ui components and prisma set up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex text-wrap px-2 py-3 text-opacity-15 shadow-md">
-        Helo
-      </div>
+    <main className="">
+      <div>Hello</div>
     </main>
   );
 }
